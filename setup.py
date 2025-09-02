0fc1170c66ed43822821ae3741c24ec0e4eb9018
suganya <sathyasugee>
2025-09-02 22:38:20 +0530

    intialized setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.1.0"
+
+REPO_NAME = "Text-Summarization-Project"
+AUTHOR_USER_NAME = "sathyasugee"
+SCR_REPO = "textSummarizer"
+AUTHOR_EMAIL = "sathya.sugee@knovatekinc.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    
+)
